d9fb47d0804d3c28db45a12db5b8c20db635641f
Emad <rasooliemad2004@gmail.com>
2025-07-20 17:16:44 +0430

    feat(core): create product page work with sanity database

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -3,32 +3,33 @@ import { PortableText, type SanityDocument } from "next-sanity";
 import { client } from "@/sanity/client";
 import Link from "next/link";
 
-const POST_QUERY = `*`;
+const POST_QUERY = `*[_type == "product"]`;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string };
+  params: { slug: string; locale?: string };
 }) {
-  const { slug } = await params;
+  const { slug, locale } = params;
   const post = await client.fetch<SanityDocument>(
     POST_QUERY,
     { slug },
     options
   );
-
-  console.log(post);
+  console.log("Post data:", post);
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
       <Link href="/" className="hover:underline">
         ← Back to posts
       </Link>
       {post.map((item: any) => (
-        <article key={item.id} className="prose">
-          <h1 className="text-3xl font-bold mb-4">{item.title}</h1>
-          <p className="text-gray-600 mb-4">{item.description}</p>
+        <article key={item._id} className="prose">
+          <h1>{item.name?.[locale ?? "en"] || item.name?.en || ""}</h1>
+          <p className="text-gray-600 mb-4">
+            {item.description?.[locale ?? "en"] || item.description?.en || ""}
+          </p>
           <PortableText value={item.content} />
         </article>
       ))}
