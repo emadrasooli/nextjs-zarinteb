5448bfffa05bbffd54e3819313f1fcf5d6be9566
Emad <rasooliemad2004@gmail.com>
2025-08-29 18:12:48 +0430

    refactor(common): refactor content of service cards

--- src/app/[locale]/service/_components/ServiceCard.tsx
+++ src/app/[locale]/service/_components/ServiceCard.tsx
@@ -14,10 +14,16 @@ export default function ServiceCard({
   return (
     <div className="bg-zinc-950 px-6 py-8 rounded-2xl border border-zinc-100 shadow-md group cursor-pointer">
       <div className="w-16 h-16 lg:w-24 lg:h-24 flex items-end">
-        <Image src={iconSrc} alt={title} width={100} height={100} />
+        <Image
+          src={iconSrc}
+          alt={title}
+          width={100}
+          height={100}
+          className="opacity-75"
+        />
       </div>
       <div className="">
-        <h3 className="text-lg font-bold text-primary mt-4">{title}</h3>
+        <h3 className="text-xl font-bold text-primary mt-4">{title}</h3>
         <p className="text-zinc-100 mt-2">{description}</p>
       </div>
     </div>
