adb2dc74e95e1a363f935970d5bfd58dc148dfe8
Emad <rasooliemad2004@gmail.com>
2025-09-28 11:22:06 +0430

    fix(common): fix import bug, delete StatsSection

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,13 +3,11 @@ import ProductList from "@/components/homePage/ProductList";
 import LogoSection from "@/components/LogoSection";
 import ServicesSection from "@/components/homePage/ServicesSection";
 import ContactCTA from "@/components/homePage/ContactCTA";
-import StatsSection from "@/components/homePage/StatsSection";
 
 export default async function Page() {
   return (
     <main>
       <Hero />
-      <StatsSection />
       <ProductList />
       <LogoSection />
       <ServicesSection />
