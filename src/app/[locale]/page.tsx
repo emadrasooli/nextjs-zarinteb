b018ce31e3179fb6f29bec308ea391ce4b9ca3ca
Emad <rasooliemad2004@gmail.com>
2025-09-28 11:28:32 +0430

    feat(common): create AboutSection on home page instead of StatsSection

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,13 @@ import ProductList from "@/components/homePage/ProductList";
 import LogoSection from "@/components/LogoSection";
 import ServicesSection from "@/components/homePage/ServicesSection";
 import ContactCTA from "@/components/homePage/ContactCTA";
+import AboutSection from "@/components/homePage/AboutSection";
 
 export default async function Page() {
   return (
     <main>
       <Hero />
+      <AboutSection />
       <ProductList />
       <LogoSection />
       <ServicesSection />
