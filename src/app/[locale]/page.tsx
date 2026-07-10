44b0d6297576041f9ae7f823585eb31805d0cf7f
M.EmadRasooli <rasooliemad2004@gmail.com>
2026-07-11 00:20:49 +0430

    refactor: comment out unused LogoSection import in page component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/homePage/Hero";
 import ProductList from "@/components/homePage/ProductList";
-import LogoSection from "@/components/LogoSection";
+// import LogoSection from "@/components/LogoSection";
 import ServicesSection from "@/components/homePage/ServicesSection";
 import ContactCTA from "@/components/homePage/ContactCTA";
 import AboutSection from "@/components/homePage/AboutSection";
